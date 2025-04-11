070137ab61c6242d6e3ecb7cee045f60720d2506
LuisMajin <majinloaizaluisfernando@gmail.com>
2025-04-11 00:22:48 -0500

    Update Batalla.java

--- Criaturitas/src/Batalla/Batalla.java
+++ Criaturitas/src/Batalla/Batalla.java
@@ -11,16 +11,19 @@ import Cri.Criatura;
  * @author majin
  */
 
+// Clase que simula una batalla entre dos criaturas
 public class Batalla {
     public static void pelear(Criatura c1, Criatura c2) {
         System.out.println("¡Comienza la batalla entre " + c1.getNombre() + " y " + c2.getNombre() + "!\n");
 
+        // Mientras ambas criaturas estén vivas, se turnan para atacarse
         while (c1.estaViva() && c2.estaViva()) {
-            c1.atacar(c2);
+            c1.atacar(c2);          // c1 ataca a c2
             if (!c2.estaViva()) break;
-            c2.atacar(c1);
+            c2.atacar(c1);          // si c2 aún vive, contraataca
         }
 
+        // Resultado de la batalla
         System.out.println("\nResultado:");
         if (c1.estaViva()) {
             System.out.println(c1.getNombre() + " gana la batalla");
@@ -29,4 +32,3 @@ public class Batalla {
         }
     }
 }
-
