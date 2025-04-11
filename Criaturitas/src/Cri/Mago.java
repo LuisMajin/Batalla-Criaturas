c77a16c861c89007abc09f1ef4d0aa5b8feebe9c
RIASLAB <117793990+RIASLAB@users.noreply.github.com>
2025-04-11 00:20:47 -0500

    Update Mago.java

--- Criaturitas/src/Cri/Mago.java
+++ Criaturitas/src/Cri/Mago.java
@@ -8,18 +8,22 @@ package Cri;
  *
  * @author Jhon Edinson R
  */
+// Clase Mago que hereda de Criatura
     public class Mago extends Criatura {
 
     public Mago(String nombre, int salud, int fuerza) {
         super(nombre, salud, fuerza);
     }
-
+        
+ // El mago ataca con hechizos
     @Override
     public void atacar(Criatura objetivo) {
         System.out.println(nombre + " lanza un hechizo a " + objetivo.getNombre());
         objetivo.defender(this.fuerza + 5);
     }
 
+        // El mago no tiene defensa especial, recibe el daño completo
+    
     @Override
     public void defender(int danio) {
         this.salud -= danio;
