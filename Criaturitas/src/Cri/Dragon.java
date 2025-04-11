34389b4a092d355eb737b90e99322a9ad668b0d7
LuisMajin <majinloaizaluisfernando@gmail.com>
2025-04-11 00:21:21 -0500

    Update Dragon.java

--- Criaturitas/src/Cri/Dragon.java
+++ Criaturitas/src/Cri/Dragon.java
@@ -8,21 +8,24 @@ package Cri;
  *
  * @author majin
  */
+// Clase Dragon que hereda de Criatura
 public class Dragon extends Criatura {
 
     public Dragon(String nombre, int salud, int fuerza) {
         super(nombre, salud, fuerza);
     }
 
+    // Ataque especial del dragón: lanza fuego (daño extra de +10)
     @Override
     public void atacar(Criatura objetivo) {
         System.out.println(nombre + " lanza fuego a " + objetivo.getNombre());
-        objetivo.defender(this.fuerza + 10); // fuego = +10
+        objetivo.defender(this.fuerza + 10); // fuego aumenta el daño
     }
 
+    // El dragón tiene escamas resistentes: recibe 5 puntos menos de daño
     @Override
     public void defender(int danio) {
-        this.salud -= danio - 5; // resistencia al fuego
+        this.salud -= danio - 5;
         System.out.println(nombre + " resistió con escamas. Salud restante: " + salud);
     }
 }
